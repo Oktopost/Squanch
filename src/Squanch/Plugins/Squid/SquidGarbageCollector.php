a5fc041edfbd859c6ff7a7b30da466b23f3861aa
Daniel Podrabinek <dpodrabinek@oktopost.com>
2017-01-31 12:24:39 +0200

    Minor update

--- src/Squanch/Plugins/Squid/SquidGarbageCollector.php
+++ src/Squanch/Plugins/Squid/SquidGarbageCollector.php
@@ -9,25 +9,60 @@ class SquidGarbageCollector
 {
 	private $connector;
 	
+	private $date;
+	private $maxIterations     = 999;
+	private $limitPerIteration = 100;
+	
+	
+	private function delete(): int
+	{
+		return $this->connector->cmdDelete()
+			->limitBy($this->limitPerIteration)
+			->where('EndDate < ?', $this->date)
+			->executeDml(true);
+	}
+
 	
 	public function __construct(ISquanchSquidConnector $connector)
 	{
 		$this->connector = $connector;
+		$this->date = new \DateTime();
 	}
 	
-	public function run(\stdClass $params = null)
+	
+	public function setMaxIterations(int $maxIterations)
 	{
-		if (isset($params->date))
+		$this->maxIterations = $maxIterations;
+		return $this;
+	}
+	
+	public function setLimitPerIteration(int $limit)
+	{
+		$this->limitPerIteration = $limit;
+		return $this;
+	}
+	
+	public function setEndDate(\DateTime $date)
+	{
+		$this->date = $date;
+		return $this;
+	}
+	
+	public function run(): int
+	{
+		$totalDeleted = 0;
+		
+		for ($i = 0; $i < $this->maxIterations; $i++)
 		{
-			$date = $params->date;
-		}
-		else
-		{
-			$date = new \DateTime();
+			$deleted = $this->delete();
+			
+			if ($deleted == 0)
+				break;
+			
+			$totalDeleted += $deleted;
+			time_nanosleep(0, 100);
 		}
 		
-		$this->connector->cmdDelete()
-			->where('EndDate < ?', $date)
-			->execute();
+		return $totalDeleted;
 	}
 }
