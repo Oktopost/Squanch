59d7716def2e6fce4fdfed961d763bd58e98258b
Daniel Podrabinek <dpodrabinek@oktopost.com>
2017-04-19 13:07:35 +0300

    Updated travis

--- db/mysql.sql
+++ db/mysql.sql
@@ -1,4 +1,22 @@
+CREATE DATABASE IF NOT EXISTS `squanch_cache`;
+
+USE `squanch_cache`;
+
 CREATE TABLE IF NOT EXISTS `HardCache` (
+    `Id` varchar(255) NOT NULL,
+    `Bucket` varchar(255) NOT NULL,
+    `Value` longtext NOT NULL,
+    `EndDate` datetime NOT NULL,
+    `Created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `Modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `TTL` int(11) NOT NULL,
+
+    PRIMARY KEY (`Id`,`Bucket`) USING BTREE,
+    
+    KEY `k_EndDate` (`EndDate`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `SoftCache` (
     `Id` varchar(255) NOT NULL,
     `Bucket` varchar(255) NOT NULL,
     `Value` longtext NOT NULL,
