cc29055b4d7a5a0436b0a5ddb2d02d8e5dd69a03
Daniel Podrabinek <dpodrabinek@oktopost.com>
2017-05-03 09:16:27 +0300

    Updated onDeleteBucket for predis plugin

--- src/Squanch/Plugins/Predis/Command/Delete.php
+++ src/Squanch/Plugins/Predis/Command/Delete.php
@@ -3,22 +3,56 @@ namespace Squanch\Plugins\Predis\Command;
 
 
 use Squanch\Commands\AbstractDelete;
-use Squanch\Exceptions\OperationNotSupportedOnBucketException;
 use Squanch\Plugins\Predis\Connector\IPredisConnector;
+
+use Predis\Collection\Iterator\Keyspace;
 
 
 class Delete extends AbstractDelete implements IPredisConnector
 {
+	const BUCKET_SIZE_TO_DELETE = 1000;
+	
+	
 	use \Squanch\Plugins\Predis\Connector\TPredisConnector;
+	
+	
+	private function delete(array $keys): bool
+	{
+		return $this->getClient()->del($keys) > 0;
+	}
 	
 	
 	protected function onDeleteBucket(string $bucket): bool
 	{
-		throw new OperationNotSupportedOnBucketException('redis::delete');
+		$iterator = new Keyspace($this->getClient(), "$bucket:*");
+		$result = false;
+		$keys = [];
+		$currentCount = 0;
+		
+		foreach ($iterator as $key)
+		{
+			$currentCount++;
+			$keys[] = $key;
+			$result = true;
+			
+			if ($currentCount == self::BUCKET_SIZE_TO_DELETE)
+			{
+				$this->delete($keys);
+				$keys = [];
+				$currentCount = 0;
+			}
+		}
+		
+		if ($currentCount > 0)
+		{
+			$this->delete($keys);
+		}
+		
+		return $result;
 	}
-
+	
 	protected function onDeleteItem(string $bucket, string $key): bool
 	{
-		return ($this->getClient()->del(["$bucket:$key"]) > 0);
+		return $this->delete(["$bucket:$key"]);
 	}
 }
